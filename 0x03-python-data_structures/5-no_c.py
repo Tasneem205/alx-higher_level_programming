5b50bed7f1222c505c7fb807c313e356c0bd4be0
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-01-08 18:31:06 -0800

    5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    n = len(my_string)
+    s = ""
+
+    for i in range(n):
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
+            s += my_string[i];
+
+    return s
