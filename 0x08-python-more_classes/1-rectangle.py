0e47e77befc31784b336ef70b9cebb01d8d83aa7
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-02-05 13:53:38 -0800

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Define a real rectangle class
+"""
+
+
+class Rectangle:
+    """Representation of rectangles"""
+    def __init__(self, width=0, height=0):
+        """initialize the rectangle"""
+        self.width = width
+        self.height = height
+
+    # property
+    def width(self):
+        """get the width"""
+        return self.__width
+
+    def width(self, value):
+        """set the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.width = value
+
+    # property
+    def height(self):
+        """get the height"""
+        return self.__height
+
+    def height(self, value):
+        """set the width"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.height = value
