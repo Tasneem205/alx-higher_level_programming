a0ec3ff3f05cdd7a6b96fe46e86c6846fbc2ab19
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-03-13 03:06:26 +0200

    15, 16

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+-- group by
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score DESC;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- select 
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+
