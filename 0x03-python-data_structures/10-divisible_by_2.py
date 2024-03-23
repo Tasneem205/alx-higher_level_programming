b5fe784a3cebc1b525e7b0cf0fe01b138bad7a24
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-03-23 16:53:16 +0200

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
+
 def divisible_by_2(my_list=[]):
     if len(my_list) == 0:
         return []
     res = []
     for i in my_list:
-        if i%2 == 0:
+        if i % 2 == 0:
             res.append(True)
         else:
             res.append(False)
