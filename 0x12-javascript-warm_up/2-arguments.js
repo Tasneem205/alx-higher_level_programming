d8e3cdf753db59a5d81b59f744766da7f0093c83
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-04-11 03:32:23 +0200

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-if (process.argv.length === 2)
-	console.log('No argument');
-else if (process.argv.length > 2)
-	console.log('Argument found');
+if (process.argv.length === 2) { console.log('No argument'); } else if (process.argv.length > 2) { console.log('Argument found'); }
