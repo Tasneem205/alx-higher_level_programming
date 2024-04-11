e5d61aa9d0fa347998ffe0d82cca0bf5f28c211b
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-04-11 22:28:43 +0200

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const x = process.argv[2];
-if (x === undefined || isNaN(x)){
+if (x === undefined || isNaN(x)) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + parseInt(x));
