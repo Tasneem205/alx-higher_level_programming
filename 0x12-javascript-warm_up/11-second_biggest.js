b358382326427c667c09cc306cd28d40972d695e
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-04-12 02:42:27 +0200

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,8 @@
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  let mx = 0, mxmx = 0;
-  for (let i  = 2; i < process.argv.length; i++) {
+  let mx = 0; let mxmx = 0;
+  for (let i = 2; i < process.argv.length; i++) {
     if (parseInt(process.argv[i]) > mx) {
       if (parseInt(process.argv[i]) > mxmx) {
         mxmx = parseInt(process.argv[i]);
