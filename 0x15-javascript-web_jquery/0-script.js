e3ed657b488c41f0f4dd01542fef1b084b3e5cab
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-05-30 05:10:45 +0300

    intial commit

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,3 +1,4 @@
+// change color using native js
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('header').style.color = '#FF0000';
 });
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,4 @@
+// change color using jQuery
+$(document).ready(function() {
+    $('header').css('color', '#FF0000');
+});
