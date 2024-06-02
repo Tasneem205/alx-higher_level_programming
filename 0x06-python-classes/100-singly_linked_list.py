f327808516042109567a8a045ebd64e8880a27b9
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-06-02 04:33:01 +0300

    advanced tasks

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""Singly linked list"""
+
+
+class Node:
+    """Class tp represent nodes"""
+    def __init__(self, data, next_node=None):
+        """init function """
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """getter & setter of data"""
+        return (self.__data)
+
+    @data.setter
+    def data(self, value):
+        """setter of data"""
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.data = value
+
+    @property
+    def next_node(self):
+        """getter of next node"""
+        return (self.__next_node)
+
+    @next_node.setter
+    def next_node(self, value):
+        """setter of next_node"""
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """singly linked list class in python"""
+
+    def __init__(self):
+        """init function """
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """sorted insertion function """
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
+            while (tmp.next_node is not not None and
+                   tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new.next_node = tmp.next_node
+            tmp.next_node = new
+
+    def __str__(self):
+        """str representation of a SLL"""
+        vals = []
+        tmp = self.__head
+        while tmp is not None:
+            vals.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ('\n'.join(vals))
---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""define a magic class"""
+
+import math
+
+
+class MagicClass:
+    """Magic class implementation"""
+
+    def __inti__(self, radius=0):
+        """init function for a circle class"""
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """area function"""
+        return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        """not the area"""
+        return (2 ** math.pi ** self.__raduis)
