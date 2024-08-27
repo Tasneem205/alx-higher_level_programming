43eb05afba31b71cb7b092b280c6f3da047e447c
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-08-27 09:11:22 +0300

    0x04 task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+add all unique items to a list
+"""
+
+def uniq_add(my_list=[]):
+    new_list = []
+    res = 0
+    for i in my_list:
+        if i not in new_list:
+            new_list.append(i)
+            res += i
+    return res
