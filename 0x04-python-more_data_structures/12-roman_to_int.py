3932f8e4fe33baa065f21fa9bbc8cccebff775e0
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-08-27 12:36:06 +0300

    0x04 task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,6 +8,8 @@ def roman_to_int(roman_string):
     dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     res = 0
     cur = 0
+    if roman_string is None or not isinstance(roman_string, str):
+        return 0
     for i in roman_string:
         value = dic.get(i, 0)
         if value > cur:
