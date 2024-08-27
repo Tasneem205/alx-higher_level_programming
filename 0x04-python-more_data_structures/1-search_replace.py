7ea03a65fde181227a435cea40ea33f1ffd73715
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-08-27 03:28:01 +0300

    python 0x04 task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+search and replace with new element
+"""
+
+def search_replace(my_list, search, replace):
+   new_list = []
+   for i in range(0, len(my_list)):
+       if my_list[i] == search:
+           new_list.append(replace)
+       else:
+           new_list.append(my_list[i])
+   return new_list
