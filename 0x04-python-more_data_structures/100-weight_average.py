6a544c62399ee34d7d064b407fd0375a46006296
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-08-27 12:49:16 +0300

    0x04 task 13, 14, 15

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+calculate wieghted average
+"""
+
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    else:
+        return (sum(a * b for a, b in my_list) / sum(b for a, b in my_list))
