045ff4e59f642702c6d83aed4a2c20758da77779
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-08-27 12:34:10 +0300

    0x04 task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+convert from roman to int
+"""
+
+
+def roman_to_int(roman_string):
+    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    res = 0
+    cur = 0
+    for i in roman_string:
+        value = dic.get(i, 0)
+        if value > cur:
+            res += value - 2 * cur
+        else:
+            res += value
+        cur = value
+    return res
